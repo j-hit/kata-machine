2d5120c2609f21eb2bf7cce3d574520961860314
James Bampoe <james.bampoe@mgb.ch>
2024-11-20 08:23:15 +0100

    feat: add quick sort solution

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,53 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], low: number, high: number) {
+    if (low >= high) {
+        return;
+    } 
 
+    const pivotIndex = partition(arr, low, high);
+
+    /**
+     * sort the left of the pivot
+     */
+    qs(arr, low, pivotIndex - 1);
+
+    /**
+     * sort the right of the pivot
+     */
+    qs(arr, pivotIndex + 1, high);
 }
+
+/**
+ * Returns the pivot index
+ * @param arr
+ * @param low 
+ * @param high 
+ */
+function partition(arr: number[], low: number, high: number): number {
+    const pivot = arr[high];
+
+    let index = low -1;
+
+    for (let i = low; i < high; i++) {
+        if (arr[i] <= pivot) {
+            index++;
+            const temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
+    }
+
+    /**
+     * Put the pivot into the pivot index
+     */
+    index++;
+    arr[high] = arr[index];
+    arr[index] = pivot;
+
+    return index;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
+}
+
+
