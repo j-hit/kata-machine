ddc2061a151ea115bdf2755acf0b52a7961da81d
James Bampoe <james.bampoe@mgb.ch>
2024-11-25 17:26:02 +0100

    feat: add breadth first search solution

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,20 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue = [head];
 
+    while(queue.length) {
+        const current = queue.shift();
+
+        if (current?.value === needle) {
+            return true;
+        }
+
+        if (current?.left){
+            queue.push(current.left);
+        }
+        if (current?.right) {
+            queue.push(current.right);
+        }
+    }
+
+    return false;
 }
