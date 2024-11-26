d7df908fe719d74412ba31f3e91d2c4aa3f1a1c3
James Bampoe <james.bampoe@mgb.ch>
2024-11-26 07:04:16 +0100

    feat: add compare binary trees solution

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,22 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+function walk(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    // structural check
+    if (a === null && b === null) {
+        return true;
+    }
 
+    // structural check
+    if (a === null || b == null){
+        return false;
+    }
+
+    // value check
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    return walk(a.left, b.left) && walk(a.right, b.right);
+}
+
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    return walk(a, b)
 }
